be70f39aadc3f23c1a559371943d6231bc62ea54
Bartosz Czechowicz <bartosz7czechowicz@gmail.com>
2020-09-10 12:32:32 +0200

    Updates

---
+++ precache-manifest.1768584ca6d4f961f39aae8c865632b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3266aae76b1ff0f3b2e72f037fb18b63",
+    "url": "/react-gallery-app/index.html"
+  },
+  {
+    "revision": "11fafe899a6061bf364d",
+    "url": "/react-gallery-app/static/css/main.ac0785fe.chunk.css"
+  },
+  {
+    "revision": "9b8744b92f8a0df31f3b",
+    "url": "/react-gallery-app/static/js/2.1eba3e82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gallery-app/static/js/2.1eba3e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fafe899a6061bf364d",
+    "url": "/react-gallery-app/static/js/main.55008804.chunk.js"
+  },
+  {
+    "revision": "61a0cf880ba30414e6ae",
+    "url": "/react-gallery-app/static/js/runtime-main.ec27dbd4.js"
+  },
+  {
+    "revision": "cc3d34d9f0a82deecadb504a487956f0",
+    "url": "/react-gallery-app/static/media/rolling.cc3d34d9.svg"
+  }
+]);
